c259bf1f6be73df1ebbc90262e051c85cda55960
Marcel Elias <marcel@dromzeh.dev>
2025-08-13 22:41:29 +0100

    chore: maintenance notice & disc easy access in navbar

--- src/components/nav/nav-bar.tsx
+++ src/components/nav/nav-bar.tsx
@@ -5,14 +5,17 @@ import Link from "next/link";
 import { ArrowRightIcon } from "lucide-react";
 import { AssetDownloadIndicator } from "../asset/download-indicator";
 import { CommandSearch } from "./command";
+import { Button } from "../ui/button";
+import { DiscordLogoIcon } from "@radix-ui/react-icons";
 
 export default function NavBar() {
     return (
         <header className="top-0 bg-background/90 backdrop-blur-sm border-b sticky z-50">
-            {/* <div className="p-2 text-xs bg-destructive hover:bg-destructive/90 justify-center text-center text-primary group flex gap-1 items-center transition-all duration-150 ease-linear">
-                wanderer.moe will be undergoing heavy updates for the next
-                couple days - the site will stay up, bugs may occur!{" "}
-            </div> */}
+            <div className="p-2 text-xs bg-destructive hover:bg-destructive/90 justify-center text-center text-primary group flex gap-1 items-center transition-all duration-150 ease-linear">
+                wanderer.moe will be undergoing maintenance for a major update
+                between Aug 16-17 for around 10 hours. Join the Discord for more
+                information & for assets missing from the site.
+            </div>
             <nav className="flex justify-between tems-center max-w-screen-xl px-5 py-2 mx-auto">
                 <Link href="/" className="text-lg font-semibold">
                     <div className="flex flex-row gap-1 items-center">
@@ -28,6 +31,15 @@ export default function NavBar() {
                 <div className="flex flex-row gap-2 items-center">
                     <AssetDownloadIndicator />
                     <CommandSearch />
+                    <Link
+                        href="https://discord.gg/noid"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <Button variant="outline">
+                            <DiscordLogoIcon className="h-4 w-4" />
+                        </Button>
+                    </Link>
                 </div>
             </nav>
         </header>
