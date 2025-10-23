541d0d1dc3eea8b49407e26fcd0bc18944bf5afc
Marcel Elias <marcel@dromzeh.dev>
2025-10-23 03:28:17 +0100

    feat: originoiiiiIIId

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { type Metadata } from 'next'
 import { UnifiedAssetList } from '~/components/asset/unified-asset-list'
-import { OriginoidPromoDialog } from '~/components/ui/originoid-promo-dialog'
 
 export const metadata: Metadata = {
     title: 'skowt.cc',
@@ -12,7 +11,6 @@ export default function Page() {
     return (
         <div className="flex flex-col p-6 min-h-screen">
             <UnifiedAssetList endpoint="/asset/search" />
-            <OriginoidPromoDialog />
         </div>
     )
 }
